673480f03505987acb3c1d7c17d940d2b6975718
aline.goncalves <aline.goncalves@db1.com.br>
2022-10-14 10:33:13 -0300

    deleting duplicated file from a wrong folder and separating updateQuality into different functions by item

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -10,6 +10,9 @@ final class GildedRose
      * @var Item[]
      */
     private $items;
+    private const AGED_BRIE = 'Aged Brie';
+    private const BACKSTAGE = 'Backstage passes to a TAFKAL80ETC concert';
+    private const SULFURAS = 'Sulfuras, Hand of Ragnaros';
 
     public function __construct(array $items)
     {
@@ -19,52 +22,92 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
+            switch ($item->name){
+                case self::AGED_BRIE:
+                    $this->updateQualityBrie($item);
+                    break;
 
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+                case self::BACKSTAGE:
+                    $this->updateQualityBackstage($item);
+                    break;
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in = $item->sell_in - 1;
-            }
+                case self::SULFURAS:
+                    $this->updateQualitySulfuras($item);
+                    break;
 
-            if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
+                default:
+                    $this->updateQualityNormal($item);
+                    break;
             }
+        }
+    }
+
+    private function updateQualityBrie(Item $item){
+        $this->increaseOneFromQualityIfItIsLessThenFifty($item);
+
+        $this->decreaseOneFromSellIn($item);
+
+        if ($item->sell_in < 0) {
+            $this->increaseOneFromQualityIfItIsLessThenFifty($item);
+        }
+    }
+
+    private function updateQualityBackstage(Item $item){
+        $this->increaseOneFromQualityIfItIsLessThenFifty($item);
+
+        if ($item->sell_in < 11) {
+            $this->increaseOneFromQualityIfItIsLessThenFifty($item);
+        }
+        if ($item->sell_in < 6) {
+            $this->increaseOneFromQualityIfItIsLessThenFifty($item);
+        }
+
+        $this->decreaseOneFromSellIn($item);
+
+        $this->decreaseQualityInZeroIfSellInIsLessThenZero($item);
+    }
+
+    private function updateQualitySulfuras(Item $item){
+        $this->increaseOneFromQualityIfItIsLessThenFifty($item);
+
+        if ($item->sell_in < 0) {
+            $this->increaseOneFromQualityIfItIsLessThenFifty($item);
+        }
+    }
+
+    private function updateQualityNormal(Item $item){
+        $this->decreaseOneFromQualityIfItIsBiggerThenZero($item);
+
+        $this->decreaseOneFromSellIn($item);
+
+        if ($item->sell_in < 0) {
+            $this->decreaseOneFromQualityIfItIsBiggerThenZero($item);
+        }
+    }
+
+    private function increaseOneFromQualityIfItIsLessThenFifty(Item $item)
+    {
+        if ($item->quality < 50) {
+            $item->quality = $item->quality + 1;
+        }
+    }
+
+    private function decreaseOneFromSellIn(Item $item)
+    {
+        $item->sell_in = $item->sell_in - 1;
+    }
+
+    private function decreaseOneFromQualityIfItIsBiggerThenZero(Item $item)
+    {
+        if ($item->quality > 0) {
+            $item->quality = $item->quality - 1;
+        }
+    }
+
+    private function decreaseQualityInZeroIfSellInIsLessThenZero(Item $item)
+    {
+        if ($item->sell_in < 0) {
+            $item->quality -= $item->quality;
         }
     }
 }
